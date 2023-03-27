71e6aec54948bf9c367904c96c1099f53a078076
Владимир Ларин <90915991+vladimir2312@users.noreply.github.com>
2023-03-27 22:48:29 +0300

    Add files via upload

---
+++ Voting/src/Admin.java
@@ -0,0 +1,11 @@
+public class Admin {
+    public void printUsers(){
+
+    }
+    public void printListCEC(){
+
+    }
+    public void createCEC(){
+
+    }
+}
---
+++ Voting/src/MainUI.java
@@ -0,0 +1,12 @@
+import  java.util.Scanner;
+public class MainUI {
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    public void menu() {
+        if (a == 1 | a == 2 | a == 3 | a == 4) {
+            if(a==1){
+                System.out.println("");
+            }
+        }
+    }
+}
